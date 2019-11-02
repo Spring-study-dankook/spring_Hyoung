16604420c696eab1eae7d3b9d6fa38d151f1181b
gudwnsgur <gudwnsgur12@naver.com>
2019-11-02 14:00:16 +0900

    LoginController에 필요한 LoginService 생성

--- src/main/java/com/dku/springstudy/controller/LoginController.java
+++ src/main/java/com/dku/springstudy/controller/LoginController.java
@@ -1,34 +1,33 @@
 package com.dku.springstudy.controller;
         import com.dku.springstudy.vo.User;
         import org.springframework.stereotype.Controller;
+        import com.dku.springstudy.service.LoginService;
         import org.springframework.ui.Model;
         import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class LoginController
 {
-
     @GetMapping("/user")
     public String user(Model model, User user)
     {
         model.addAttribute("id", user.getUser_Id());
         model.addAttribute("pwd", user.getUser_Pwd());
-        return "CheckUserView";
+        return "UserView";
     }
 
     @PostMapping("/login")
-    public String Login (Model model, User user,
+    public String Login (Model model, User user, LoginService loginService,
                          @RequestParam(value = "inputId" , required=false) String inputId ,
                          @RequestParam(value = "inputPwd" , required=false) String inputPwd )
     {
         model.addAttribute("id", inputId);
         model.addAttribute("pwd", inputPwd);
 
-        if (  user.getUser_Id().equals(inputId)  &&
-                user.getUser_Pwd().equals(inputPwd)  )
-            return "LoginView";
+        if( loginService.isLoginSucceed(user, inputId, inputPwd) )
+            return "LoginSucceededView";
         else
-            return "LoginView";
+            return "LoginFailedView";
     }
 }
 
--- src/main/java/com/dku/springstudy/service/LoginService.java
+++ src/main/java/com/dku/springstudy/service/LoginService.java
@@ -1,8 +1,19 @@
 package com.dku.springstudy.service;
+import com.dku.springstudy.vo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class LoginService {
+public class LoginService
+{
+
+    public boolean isLoginSucceed (User user, String inputId, String inputPwd) {
+        if (  user.getUser_Id().equals(inputId)  &&
+                user.getUser_Pwd().equals(inputPwd)  )
+            return true;
+        else
+            return false;
+    }
+
 }
